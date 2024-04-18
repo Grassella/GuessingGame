f4c238290abace655a77786e581297c7a56e1236
grassella <negrete.graciela@yahoo.com>
2024-04-17 23:26:22 -0500

    phase 1 done

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -5,4 +5,45 @@ const rl = readline.createInterface({
    output: process.stdout,
  })
 
+let secretNumber = 2;
+
+function checkGuess(num) {
+    if (num > secretNumber) {
+        console.log("Too high")
+        return false;
+    }
+    if (num < secretNumber) {
+        console.log("Too low")
+        return false;
+    } 
+    if (num === secretNumber){
+        console.log("Correct!")
+        return true;
+    }
+   
+}
+
+function askGuess() {
+    rl.question("Enter a guess: ", (answer)  => {
+   
+    Number(answer)
+    if (!checkGuess(answer)) {
+        askGuess();
+    }
+    else {
+        console.log("You win!")
+        rl.close()
+    }
+
+    });
  
+}
+
+askGuess()
+
+
+
+
+
+
+
