29bb6fc096d9c06a9602da8e2c5487828b7f172c
Kayleen Sandoval <kayleen.sandoval02@gmail.com>
2024-04-18 22:48:45 -0500

    SOLVED GUESSING GAMEgit add guessing-game.js

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -5,27 +5,27 @@ const rl = readline.createInterface({
    output: process.stdout,
  })
 
-let secretNumber = 2;
+  let secretNumber;
 
 function checkGuess(num) {
-    if (num > secretNumber) {
+    if (Number(num) > secretNumber) {
         console.log("Too high")
         return false;
     }
-    if (num < secretNumber) {
+    if (Number(num) < secretNumber) {
         console.log("Too low")
         return false;
-    } 
-    if (num === secretNumber){
+    }
+    if (Number(num) === secretNumber){
         console.log("Correct!")
         return true;
     }
-   
+
 }
 
 function askGuess() {
     rl.question("Enter a guess: ", (answer)  => {
-   
+
     Number(answer)
     if (!checkGuess(answer)) {
         askGuess();
@@ -36,14 +36,33 @@ function askGuess() {
     }
 
     });
- 
+
 }
 
-askGuess()
+//askGuess()
 
 
+function randomInRange(min, max){
+    // min = Number > 0
+    // max = Number > 0
+   const minCeiled = Math.ceil(min)
+   const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
 
+function askRange(){
+    rl.question('Enter a max number: ', (max) => {
+        max = Number(max)
+      if (max){
+    rl.question('Enter a min number: ', (min) => {
+    console.log("I am thinking of a number between " + min + " and " +  max + "...")
+    min = Number(min)
+    secretNumber = randomInRange(min, max)
+    askGuess(secretNumber)
 
-
-
-
+    //  console.log(randomInRange(min, max))
+       })
+     }
+ })
+}
+askRange()
